10b45686365ac2fd62796fed01d4ab152aea1415
David He <he@stetter-itq.com>
2022-06-02 17:45:28 +0100

    'gui_commit_for_linux_testing'

--- logsim/gui.py
+++ logsim/gui.py
@@ -7,7 +7,9 @@ Classes:
 MyGLCanvas - handles all canvas drawing operations.
 Gui - configures the main window and all the widgets.
 """
+import os
 import wx
+import wx.lib.scrolledpanel as scrolled
 import wx.glcanvas as wxcanvas
 from OpenGL import GL, GLUT
 
@@ -17,7 +19,7 @@ from network import Network
 from monitors import Monitors
 from scanner import Scanner
 from parse import Parser
-
+from userint import UserInterface
 
 class MyGLCanvas(wxcanvas.GLCanvas):
     """Handle all drawing operations.
@@ -258,6 +260,130 @@ class MyGLCanvas(wxcanvas.GLCanvas):
         GL.glEnd()
         return
 
+class scrolledpanel(scrolled.ScrolledPanel):
+    """Configures the scrolled panel on the left to hold all switches"""
+    def __init__(self,parent):
+        scrolled.ScrolledPanel.__init__(self,parent,size=(200,170))
+
+        self.label1 = wx.StaticText(self, label = "Switch 1")
+        self.label2 = wx.StaticText(self, label = "Switch 2")
+        self.label3 = wx.StaticText(self, label = "Switch 3")
+        self.label4 = wx.StaticText(self, label = "Switch 4")
+        self.label5 = wx.StaticText(self, label = "Switch 5")
+        self.label6 = wx.StaticText(self, label = "Switch 6")
+        self.label7 = wx.StaticText(self, label = "Switch 7")
+
+        self.button1 = wx.ToggleButton(self,label = "On 1")
+        self.button2 = wx.ToggleButton(self,label = "On 1")
+        self.button3 = wx.ToggleButton(self,label = "On 1")
+        self.button4 = wx.ToggleButton(self,label = "On 1")
+        self.button5 = wx.ToggleButton(self,label = "On 1")
+        self.button6 = wx.ToggleButton(self,label = "On 1")
+        self.button7 = wx.ToggleButton(self,label = "On 1")
+
+        self.button1.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button2.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button3.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button4.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button5.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button6.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+        self.button7.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleClick)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        firstsizer = wx.BoxSizer(wx.HORIZONTAL)
+        secondsizer = wx.BoxSizer(wx.HORIZONTAL)
+        thirdsizer = wx.BoxSizer(wx.HORIZONTAL)
+        fourthsizer = wx.BoxSizer(wx.HORIZONTAL)
+        fifthsizer = wx.BoxSizer(wx.HORIZONTAL)
+        sixthsizer = wx.BoxSizer(wx.HORIZONTAL)
+        seventhsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        sizer.Add(firstsizer,0, wx.ALL, 5)
+        sizer.Add(secondsizer,0, wx.ALL, 5)
+        sizer.Add(thirdsizer,0, wx.ALL, 5)
+        sizer.Add(fourthsizer,0, wx.ALL, 5)
+        sizer.Add(fifthsizer,0, wx.ALL, 5)
+        sizer.Add(sixthsizer,0, wx.ALL, 5)
+        sizer.Add(seventhsizer,0, wx.ALL, 5)
+
+        firstsizer.Add(self.label1, 1, wx.TOP, 10)
+        firstsizer.Add(self.button1, 1, wx.TOP, 10)
+        secondsizer.Add(self.label2, 1, wx.TOP, 10)
+        secondsizer.Add(self.button2, 1, wx.TOP, 10)
+        thirdsizer.Add(self.label3, 1, wx.TOP, 10)
+        thirdsizer.Add(self.button3, 1, wx.TOP, 10)
+        fourthsizer.Add(self.label4, 1, wx.TOP, 10)
+        fourthsizer.Add(self.button4, 1, wx.TOP, 10)
+        fifthsizer.Add(self.label5, 1, wx.TOP, 10)
+        fifthsizer.Add(self.button5, 1, wx.TOP, 10)
+        sixthsizer.Add(self.label6, 1, wx.TOP, 10)
+        sixthsizer.Add(self.button6, 1, wx.TOP, 10)
+        seventhsizer.Add(self.label7, 1, wx.TOP, 10)
+        seventhsizer.Add(self.button7, 1, wx.TOP, 10)
+
+        self.SetSizer(sizer)
+        self.SetupScrolling()
+
+    def onToggleClick(self, event):
+        state = event.GetEventObject().GetValue()
+
+        if state == True:
+            #self.Label.SetLabel("Off")
+            event.GetEventObject().SetLabel("Off 0")
+
+        else:
+            #self.label.SetLabelText("On")
+            event.GetEventObject().SetLabel("On 1")
+
+class scrolledpanel2(scrolled.ScrolledPanel):
+    """Configures the scrolled panel on the left to hold all switches"""
+    def __init__(self,parent):
+        scrolled.ScrolledPanel.__init__(self,parent,size=(200,170))
+        listofmonitors = ["A","B","C","D","E"]
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        for n in range(len(listofmonitors)):
+            a = str(n+1)
+            b = 'button' + str(n+1)
+            c = 'label' + str(n+1)
+            a = wx.BoxSizer(wx.HORIZONTAL)
+            sizer.Add(a,0, wx.ALL, 5)
+            self.c = wx.StaticText(self, label = listofmonitors[n])
+            self.a = wx.Button(self, wx.ID_ANY, label=b)
+            a.Add(self.c,1, wx.TOP, 10)
+            a.Add(self.a,1, wx.TOP, 10)
+
+        '''for n in range(len(listofmonitors)):
+            a = 'button' + str(n)
+            self.a = wx.Button(self, wx.ID_ANY, label=a)
+            b = str(n)
+            b.Add(self.a,1, wx.TOP, 10)'''
+            
+            #self.n = wx.StaticText(self, wx.ID_ANY, "Cycles:")
+
+        '''for n in range(len(listofmonitors)):
+            a = str(n)
+            sizer.Add(self.a,0, wx.ALL, 5)'''
+
+        self.SetSizer(sizer)
+        self.SetupScrolling()
+
+    def makeSizer(self,sizername):
+        a = str(sizername)
+        a_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+    def onToggleClick(self, event):
+        state = event.GetEventObject().GetValue()
+
+        if state == True:
+            #self.label.SetLabelText("Off")
+            event.GetEventObject().SetLabel("Off 0")
+
+        else:
+            #self.label.SetLabeText("On")
+            event.GetEventObject().SetLabel("On 1")
+
 class Gui(wx.Frame):
     """Configure the main window and all the widgets.
 
@@ -285,53 +411,96 @@ class Gui(wx.Frame):
         """Initialise widgets and layout."""
         super().__init__(parent=None, title=title, size=(800, 600))
 
+        #changing the initialisation parameters
+        #self.SetBackgroundColour((100, 200, 100))
+
         # Configure the file menu
-        fileMenu = wx.Menu()
-        menuBar = wx.MenuBar()
-        fileMenu.Append(wx.ID_ABOUT, "&About")
+        fileMenu = wx.Menu()                       #this adds in a "menu" button
+        menuBar = wx.MenuBar()                     #this adds in a menubar which displays buttons such as "file" and maybe "edit"
+        fileMenu.Append(wx.ID_ABOUT, "&About")     #the ID is specified so when the "About" button is pressed, that ID is called
+        fileMenu.Append(wx.ID_OPEN, "&Open File...") #what does the & do here?
+        fileMenu.Append(wx.ID_HELP, "&Help")
         fileMenu.Append(wx.ID_EXIT, "&Exit")
         menuBar.Append(fileMenu, "&File")
         self.SetMenuBar(menuBar)
 
         # Canvas for drawing signals
-        self.canvas = MyGLCanvas(self, devices, monitors, network)
+        self.canvas = MyGLCanvas(self, devices, monitors)
 
         # Configure the widgets
-        self.text = wx.StaticText(self, wx.ID_ANY, "Cycles")
+        self.text = wx.StaticText(self, wx.ID_ANY, "Cycles:")
+        font = wx.Font(10,wx.DECORATIVE, wx.NORMAL, wx.BOLD, True)
+        self.text.SetFont(font)
         self.spin = wx.SpinCtrl(self, wx.ID_ANY, "10")
         self.run_button = wx.Button(self, wx.ID_ANY, "Run")
+        self.text2 = wx.StaticText(self, wx.ID_ANY, "Text based control:")
+        self.text2.SetFont(font)
+        self.text_box_command = wx.TextCtrl(self, wx.ID_ANY, "",style=wx.TE_PROCESS_ENTER) 
+        self.text_box_command.SetHint('Type function here')
         self.text_box = wx.TextCtrl(self, wx.ID_ANY, "",
-                                    style=wx.TE_PROCESS_ENTER)
-
-        # Bind events to widgets
+                                    style=wx.TE_PROCESS_ENTER|wx.TE_MULTILINE|wx.TE_READONLY|wx.HSCROLL)
+        self.clear_button = wx.Button(self, wx.ID_ANY, "clear")
+        self.text3 = wx.StaticText(self, wx.ID_ANY, "Switches:")
+        self.text3.SetFont(font)
+        self.text4 = wx.StaticText(self, wx.ID_ANY, "Monitors:")
+        self.text4.SetFont(font)
+        
+        # Bind events to widgets                #this assigns the functions interacting with the widgets will actually do
         self.Bind(wx.EVT_MENU, self.on_menu)
         self.spin.Bind(wx.EVT_SPINCTRL, self.on_spin)
         self.run_button.Bind(wx.EVT_BUTTON, self.on_run_button)
+        self.run_button.Disable()
+        self.text_box_command.Bind(wx.EVT_TEXT_ENTER, self.on_text_box_command)
         self.text_box.Bind(wx.EVT_TEXT_ENTER, self.on_text_box)
-
+        self.clear_button.Bind(wx.EVT_BUTTON, self.on_clear_button)
+        
         # Configure sizers for layout
-        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        side_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer = wx.BoxSizer(wx.HORIZONTAL)     
+        right_sizer = wx.BoxSizer(wx.VERTICAL)
+        bottom_sizer = wx.BoxSizer()
+        left_sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        #main_sizer.Add(self.canvas, 5, wx.EXPAND | wx.ALL, 5)  
+        main_sizer.Add(left_sizer, 0, wx.ALL, 5)                       #the first number represents the proportion and the second the border
+        main_sizer.Add(right_sizer, 5, wx.EXPAND|wx.ALL, 5) 
+        right_sizer.Add(self.canvas, 10, wx.EXPAND | wx.ALL, 5)
+        right_sizer.Add(bottom_sizer,5, wx.EXPAND | wx.ALL, 5)                      
+        
+        left_sizer.Add(self.text, 1, wx.TOP, 1)
+        left_sizer.Add(self.spin, 1, wx.ALL, 1)
+        left_sizer.Add(self.text2, 1, wx.TOP, 5)
+        left_sizer.Add(self.text_box_command, 1, wx.ALL, 1)
+        left_sizer.Add(self.text3, 1, wx.TOP, 5)
+        left_sizer.Add(scrolledpanel(self))
+        left_sizer.Add(self.text4, 1, wx.TOP, 5)
+        left_sizer.Add(scrolledpanel2(self))
+        left_sizer.Add(self.clear_button, 1, wx.ALL, 1)
+        left_sizer.Add(self.run_button, 1, wx.ALL, 1)
+        bottom_sizer.Add(self.text_box,1, wx.EXPAND|wx.ALL, 5)         #here changing the first number to 0 holds the width fixed and changing it to 1 allows it to stretch as window is resized
 
-        main_sizer.Add(self.canvas, 5, wx.EXPAND | wx.ALL, 5)
-        main_sizer.Add(side_sizer, 1, wx.ALL, 5)
-
-        side_sizer.Add(self.text, 1, wx.TOP, 10)
-        side_sizer.Add(self.spin, 1, wx.ALL, 5)
-        side_sizer.Add(self.run_button, 1, wx.ALL, 5)
-        side_sizer.Add(self.text_box, 1, wx.ALL, 5)
-
-        self.SetSizeHints(600, 600)
+        self.SetSizeHints(800, 800)
         self.SetSizer(main_sizer)
 
     def on_menu(self, event):
         """Handle the event when the user selects a menu item."""
-        Id = event.GetId()
+        Id = event.GetId()                                                 #now the event.GetId() simplies finds out which button was clicked
         if Id == wx.ID_EXIT:
-            self.Close(True)
+            dlg = wx.MessageDialog(self,"Are you sure you want to exit?","Confirm exit",wx.CANCEL | wx.OK)
+            result = dlg.ShowModal()
+            dlg.Destroy()
+            if result == wx.ID_CANCEL:
+                print("The user cancelled")
+            if result == wx.ID_OK:
+                self.Close(True)
         if Id == wx.ID_ABOUT:
             wx.MessageBox("Logic Simulator\nCreated by Mojisola Agboola\n2017",
                           "About Logsim", wx.ICON_INFORMATION | wx.OK)
+        if Id == wx.ID_OPEN:
+            self.on_open(True)
+        if Id == wx.ID_HELP:
+            #a = str(UserInterface.help_command(self))
+            self.text_box.WriteText(str(UserInterface.help_command(self)))
+            self.text_box.WriteText("\n\n")
 
     def on_spin(self, event):
         """Handle the event when the user changes the spin control value."""
@@ -342,6 +511,44 @@ class Gui(wx.Frame):
     def on_run_button(self, event):
         """Handle the event when the user clicks the run button."""
         text = "Run button pressed."
+        self.canvas.render(text)
+        #Devices.make_switch(self, device_id, initial_state)
+
+    def on_text_box_command(self, event):
+        "Handle the event when the user enters text."
+        text_box_command_value = self.text_box_command.GetValue()
+        text = "".join(["New text box value: ", text_box_command_value])
+        self.canvas.render(text)
+        if text_box_command_value == "q":
+            dlg = wx.MessageDialog(self,"Are you sure you want to exit?","Confirm exit",wx.CANCEL | wx.OK)
+            result = dlg.ShowModal()
+            dlg.Destroy()
+            if result == wx.ID_CANCEL:
+                print("The user cancelled")
+            if result == wx.ID_OK:
+                self.Close(True)
+         
+
+    def on_open(self, event):
+        openFileDialog= wx.FileDialog(self, "Open txt file", "", "", wildcard="TXT files (*.txt)|*.txt", style=wx.FD_OPEN+wx.FD_FILE_MUST_EXIST)
+
+        if openFileDialog.ShowModal() == wx.ID_CANCEL:
+            print("The user cancelled") 
+            return     # the user changed idea...
+        print("File chosen=",openFileDialog.GetPath())
+        
+        path = openFileDialog.GetPath()
+        if os.path.exists(path):
+            with open(path) as fobj:
+                for line in fobj:
+                    self.text_box.WriteText(line)
+            self.text_box.WriteText("\n\n")
+            print(path)
+
+    def on_clear_button(self, event):
+        "Handle the event when the user clicks the clear button"
+        self.text_box.SetValue("")
+        text = "clear button pressed"
         self.canvas.render(text)
 
     def on_text_box(self, event):
