e3ad929a319399a6113317deef7e78b1da4313c7
Harrison Zheng <zhengjingyaoharrison@gmail.com>
2022-06-02 01:04:24 +0100

    📍

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -12,7 +12,7 @@ from parse import Parser
 
 def start_up(path):
     names = Names()
-    scanner = Scanner(path, names, True)
+    scanner = Scanner(path, names, False)
     devices = Devices(names)
     network = Network(names, devices)
     monitors = Monitors(names, devices, network)
@@ -21,7 +21,7 @@ def start_up(path):
 
 @pytest.fixture
 def parser():
-    return start_up(../Definition_file.txt)
+    return start_up("Definition_file.txt")
 @pytest.mark.parametrize("input, output",
                         [("Device{} Connection{} ", False),
                          ("Device{} Connection{} Monitor", True)
@@ -30,12 +30,12 @@ def parser():
     
 )
 
-def test_parse_netwrok(input,output):
-    indicate = False
-    parse = start(input)
-    parse.parse_network()
-    if (new_parse.device_section == True and
-            new_parse.connection_section == True and
-                new_parse.monitor_section == True):
-        indicate = True
-    assert indicate == output
+# def test_parse_netwrok(input,output):
+#     indicate = False
+#     parse = start(input)
+#     parse.parse_network()
+#     if (new_parse.device_section == True and
+#             new_parse.connection_section == True and
+#                 new_parse.monitor_section == True):
+#         indicate = True
+#     assert indicate == output
